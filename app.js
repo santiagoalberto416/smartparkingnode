680a2f7590edf09a1e4c00de76dbecb0ca09e4aa
santy_ruler <santiagoalberto416@gmail.com>
2017-11-16 18:26:38 +0000

    removed functions that they didn't work

--- app.js
+++ app.js
@@ -46,26 +46,26 @@ function removeUser(idUser){
 
 //-----------------------------------------------------------------------------------------------------------
 
+//Connection
+var con = mysql.createConnection({
+  host: "5.79.67.193",
+  user: "db_u1403",
+  password: "BNmXfvwHd",
+  database: "parking"
+});
 
-  var con = mysql.createConnection({
-    host: "5.79.67.193",
-    user: "db_u1403",
-    password: "BNmXfvwHd",
-    database: "parking"
-  });
+conect();
 
-  conect();
-
-	//server.listen(process.env.PORT, process.env.IP);
-	server.listen(8082, function(){
-	  console.log('listening on *:8082');
-	});
-	app.use(express.static('web'));
+//server.listen(process.env.PORT, process.env.IP);
+server.listen(8082, function(){
+  console.log('listening on *:8082');
+});
+app.use(express.static('web'));
 
 
-	app.get('/', function(req, res){
-	 res.sendFile(__dirname + '/web/display.html');
-	});
+app.get('/', function(req, res){
+ res.sendFile(__dirname + '/web/display.html');
+});
 
 
 io.on('connection', function(socket){
@@ -77,11 +77,6 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     io.emit('some event', { for: 'everyone' });
-  });
-
-  socket.on('refreshTrend', function(){
-    io.emit('sendSignalRefesh', '');
-    console.log('puto socket :v')
   });
 
   socket.on('registerUser', function(user){
@@ -108,7 +103,6 @@ io.on('connection', function(socket){
 			// se emite a el id del usuario o del dispositivo que se dio de alta
 		  io.emit("socketUser"+idOfUser, "user with id "+idOfUser+" has gone out");
 		}
-
   });
 });
 
@@ -126,36 +120,31 @@ function conect(){
 
 function insert(id, state) {
   console.log('Soy la función insert');
-    if(connected){
-          var numState = 0;
-          console.log('state: ' + state)
-          if(state == "free"){
-            numState = 1;
-          } else if (state == "ocupated"){
-            numState = 2;
-          }else{
-            return;
-          }
-          var sql = "call sp_add_activity("+id+","+numState+","+1+")";
-          con.query(sql, function (err, result) {
-          if (err) throw err;
-
-          console.log("1 record inserted");
-
-          var update = "call sp_add_activity("+id+","+numState+","+2+")";
-
-          con.query(update, function(err, result){
-            if(err) throw err;
-            io.emit('refreshTrend', '');
-            console.log('update succesful');
-          });
-
-        });
+  if(connected){
+    var numState = 0;
+    console.log('state: ' + state)
+    if(state == "free"){
+      numState = 1;
+    } else if (state == "ocupated"){
+      numState = 2;
     }else{
-        conect();
-        console.log("trying to reconect to database");
+      return;
     }
-
+    var sql = "call sp_add_activity("+id+","+numState+","+1+")";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+      var update = "call sp_add_activity("+id+","+numState+","+2+")";
+      con.query(update, function(err, result){
+        if(err) throw err;
+        io.emit('refreshTrend', '');
+        console.log('update succesful');
+      });
+    });
+  }else{
+    conect();
+    console.log("trying to reconect to database");
+  }
 }
 
 /// to restart service
